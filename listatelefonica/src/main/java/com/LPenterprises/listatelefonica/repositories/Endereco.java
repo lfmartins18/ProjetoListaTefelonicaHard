d1563cfd8bbbfeee18295fb569c8754ab8258a23
Luiz Fernando <luiz18659@gmail.com>
2018-10-30 15:31:35 -0300

    Corrigindo o erro nas interface por falta do mapeamento de repository

--- listatelefonica/src/main/java/com/LPenterprises/listatelefonica/repositories/Endereco.java
+++ listatelefonica/src/main/java/com/LPenterprises/listatelefonica/repositories/Endereco.java
@@ -1,7 +1,9 @@
 package com.LPenterprises.listatelefonica.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface Endereco extends JpaRepository<Endereco, Long>{
 
 }
--- listatelefonica/src/main/java/com/LPenterprises/listatelefonica/repositories/ListaBlockRepository.java
+++ listatelefonica/src/main/java/com/LPenterprises/listatelefonica/repositories/ListaBlockRepository.java
@@ -1,9 +1,11 @@
 package com.LPenterprises.listatelefonica.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.LPenterprises.listatelefonica.model.ListaBlock;
 
+@Repository
 public interface ListaBlockRepository extends JpaRepository<ListaBlock, Long>{
 
 }
