8c60bbce3535563c87cef405a9a0b3958acd8035
pauloizd <paulosp1995@gmail.com>
2018-10-30 13:49:58 -0300

    class ListaBlockRepository

---
+++ listatelefonica/src/main/java/com/LPenterprises/listatelefonica/repositories/ListaBlockRepository.java
@@ -0,0 +1,9 @@
+package com.LPenterprises.listatelefonica.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.LPenterprises.listatelefonica.model.ListaBlock;
+
+public interface ListaBlockRepository extends JpaRepository<ListaBlock, Long>{
+
+}
